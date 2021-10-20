38a890e2d6a5fca019c9a379a6feef5e550c3831
fajar1533 <fjrhdytlh22@gmail.com>
2021-10-20 14:54:40 +0700

    upload fjr

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function myFunction(x) {
+  if (getElementid) { // If media query matches
+    
+}
+
+var x = window.matchMedia("(max-width: 700px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
